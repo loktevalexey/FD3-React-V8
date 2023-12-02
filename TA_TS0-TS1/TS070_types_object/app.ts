cc39a3398463aba6bcafece21e5363d5e7f163e3
loktev <loktev@tut.by>
2023-12-02 15:24:57 +0300

    lesson

--- TA_TS0-TS1/TS070_types_object/app.ts
+++ TA_TS0-TS1/TS070_types_object/app.ts
@@ -5,15 +5,21 @@ console.log(friend);
 
 friend.fam="Петров";
 // friend.fam2="Сидоров"; // ошибка
-
-{
-    let friendKey:keyof {fam:string,im:string,otch:string,age:number};
-    friendKey="fam";
-    // friendKey="fam2"; // ошибка
-}
+// friend.fam=222;
 
 {
     let friendKey:keyof typeof friend;
+    friendKey="fam";
+    //friendKey="fam2"; // ошибка
+
+    //let keyname=prompt("введите имя ключа");
+    //friend[keyname]="newval";
+    friend[friendKey]="sss";
+    // friend[friendKey]=5; // ошибка
+}
+
+{
+    let friendKey:keyof {fam:string,im:string,otch:string,age:number};
     friendKey="fam";
     // friendKey="fam2"; // ошибка
 }
@@ -22,3 +28,4 @@ friend.fam="Петров";
 
 let father:Record<string,any>={}; // т.е. ключом будет строка, значением - что угодно
 father.fio="Сидоров Сидор Сидорович";
+
