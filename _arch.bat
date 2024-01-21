8c2c856f14ba81ce6169ed65487a5d220cdc0206
loktev <loktev@tut.by>
2024-01-21 18:19:40 +0300

    cloned

--- _arch.bat
+++
@@ -1,2 +0,0 @@
-rar a -s -r -m5 -sfx -x.git -x.git/*.* FD3-React-V8
-pause
--- _init.bat
+++ _init.bat
@@ -2,6 +2,6 @@ git init
 git config credential.helper store
 git add .
 git commit -m "cloned"
-git remote add origin https://github.com/loktevalexey/FD3-React-V8.git
+git remote add origin https://github.com/loktevalexey/FD3-React-V9.git
 git push -u origin master
 git status
